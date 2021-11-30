fc30f534b0f00044e7bdc9f4eb6a23b6c13e1012
Enrico03Muller <91874302+Enrico03Muller@users.noreply.github.com>
2021-11-30 12:30:16 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,23 @@
+//
+// Created by Enrico Muller on 29/11/2021.
+//
 
+char funzione (char a){
+    if (a >= 97 && a <= 122 || a >= 65 && a <= 90){ //lettera
+        return 0;
+    }else{
+        return 1;
+    }
+}
+
+char convertitore (char a){
+    char risultato;
+    if (a >= 97 && a <= 122){ //minuscola
+        //diventa maiuscola
+        risultato = (a - 32);
+    }else { //maiuscola
+        //diventa minuscola
+       risultato = (a + 32) ;
+    }
+    return risultato;
+}
