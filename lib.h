0eeaf791e9dcbeca9ccbf488e83349878e9aa344
Enrico03Muller <91874302+Enrico03Muller@users.noreply.github.com>
2021-11-30 12:30:48 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,11 @@
+//
+// Created by Enrico Muller on 29/11/2021.
+//
+
+#ifndef ESERCIZIO6_0_PRIMALIBRERIA_H
+#define ESERCIZIO6_0_PRIMALIBRERIA_H
+char funzione (char);
+char convertitore (char);
+#endif //ESERCIZIO6_0_PRIMALIBRERIA_H
+
 
